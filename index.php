92b2e3beff9592cfb821d9c0be81fcf2373ceb6a
Deysicorrales06 <76816143@idexperujapon.edu.pe>
2024-10-02 17:58:41 -0500

    EN LINEA

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./resources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./resources/css/bootstrap-icons/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="./resources/css/main.css">
+</head>
+
+<body class="px-3">
+
+    <!--encabezado -->
+    <div class="row mt-5">
+        <div class="col-sm-12 col-md-6">
+            <h1 class="text-center">EN LÍNEA</h1>
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <p class="text-rigth pr-5">
+                SOLUCIONES MUNDIALES
+            </p>
+        </div>
+        </di>
+        <!-- fin encabezado -->
+        <div class="row">
+            <div class="col-sm-12">
+                <img class="w-100" style="height: 850px;" src="./resources/img/baner01.PNG" alt="">
+            </div>
+        </div>
+        <div class="container mt-4">
+            <h1> SOLUCIONES CREATIVAS </h1>
+            <p> Sin importar cual sea tu negocio, nuestro equipo puede hacerlo desde cero.</p>
+            <div class="row">
+                <div class="col-sm-12 col-md-4">
+                    <div class="car-body">
+                        <img class="w-100" src="./resources/img/idea_01.PNG" alt="">
+                        <h5 class=h3>
+                            <i class="bi bi-pen"></i>DISEÑO
+                        </h5>
+                        <p class="pt-0">Creamos diseño segun tus necesidades.</p>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <div clas="card boder-0">
+                        <img class="w-100" src="./resources/img/idea_02.PNG" alt="">
+                        <h5 class="h5 mt-2">
+                            <i class="bi bi-flower1"></i> DESARROLLO
+                        </h5>
+                        <p>Nuestros Programadores te garantizaran la perfeccion.</p>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <div clas="card boder-0">
+                        <img class="w-100" src="./resources/img/idea_03.PNG" alt="">
+                        <h5 class="h5 mt-2">
+                            <i class="bi bi-plus-square-fill"></i> EDICION
+                        </h5>
+                        <p>Que el producto final llegue a tus clientes .</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <footer class="bg-wine">
+                <p class="p-5 text-white">Edificio IDEX PJ, jr.
+                    Amazonas 120 - Chachapoyas Chachapoyas - Amazonas.
+                </p>
+            </footer>
+</body>
+
+</html>
